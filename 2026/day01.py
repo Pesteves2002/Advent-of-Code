8c3681a0005af3a1b496cb6e9bf4fc4dbebf82e8
Tomás Esteves <tomasesteves2002@gmail.com>
2025-12-02 10:00:06 +0000

    feat: improve day01 and add input directory

--- 2026/day01.py
+++ 2026/day01.py
@@ -1,34 +1,62 @@
 import sys
 
-filepath = sys.argv[1]
 
-f = open(filepath)
+def part_1(start: int, window_size: int, actions: list[str]):
+    count = 0
+    pointer = start
+    for action in actions:
+        dir = action[0]
+        num = int(action[1:-1])
 
-part_2 = len(sys.argv) >= 3
+        if dir == "L":
+            pointer = (pointer - num) % window_size
 
-actions = f.readlines()
-
-pointer = 50
-window_size = 100
-
-count = 0
-for action in actions:
-    dir = action[0]
-    num = int(action[1:-1])
-
-    if dir == "L":
-        if (pointer - num) < 0:
-            count += abs((window_size + pointer - num) // window_size)
-            if pointer != 0:
-                count += 1
-
-        pointer = (pointer - num) % window_size
+        if dir == "R":
+            pointer = (pointer + num) % window_size
 
         if pointer == 0:
             count += 1
 
-    if dir == "R":
-        count += (pointer + num) // window_size
-        pointer = (pointer + num) % window_size
+    return count
 
-print("count:", count)
+
+def part_2(start:int , window_size: int, actions: list[str]):
+    count = 0
+    pointer = start
+    for action in actions:
+        dir = action[0]
+        num = int(action[1:-1])
+
+        if dir == "L":
+            if (pointer - num) < 0:
+                count += abs((window_size + pointer - num) // window_size)
+                if pointer != 0:
+                    count += 1
+
+            pointer = (pointer - num) % window_size
+
+            if pointer == 0:
+                count += 1
+
+        if dir == "R":
+            count += (pointer + num) // window_size
+            pointer = (pointer + num) % window_size
+
+    return count
+
+def main():
+
+    filepath = sys.argv[1]
+
+    f = open(filepath)
+
+    actions = f.readlines()
+
+    start = 50
+    window_size = 100
+
+    print("Part_1:", part_1(start, window_size, actions))
+    print("Part_2:", part_2(start, window_size, actions))
+
+if __name__ == "__main__":
+    main()
